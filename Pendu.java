63df107376769b2055615155650d2323d6b7106d
eje019 <emricndayake@gmail.com>
2026-03-06 15:02:41 +0000

    Feat : liste de mots accessible a toutes les methodes de la classe

---
+++ Pendu.java
@@ -0,0 +1,15 @@
+public class Pendu {
+    private String[] listeMots = {
+        "maison", 
+        "ordinateur", 
+        "java", 
+        "programmation", 
+        "fenetre", 
+        "bouton", 
+        "souris", 
+        "clavier", 
+        "ecran", 
+        "jeu"
+    };
+}
+
