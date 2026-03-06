52e325cce11eb1ac930aa85aa9d025bb465674fe
eje019 <emricndayake@gmail.com>
2026-03-06 16:21:58 +0000

    Feat : ajout de la logique de sélection aléatoire du mot secret et initialisation du mot masqué dans la classe Pendu

--- Pendu.java
+++ Pendu.java
@@ -18,10 +18,24 @@ public class Pendu {
 
     private String motMasque;
 
-    public Pendu() {
-        
-    }
+    private char[] lettresProposees = new char[26];
 
+    private int nbLettresProposees = 0;
+
+    public Pendu() {
+
+    //on veut choisir un mot au hasard dans listeMots
+
+    int index = (int)(Math.random() * listeMots.length);
+
+    String MotMasque = "";
+
+    motSecret = listeMots[index];
+
+    for (int i = 0; i < motSecret.length() -1 ; i++)
+        motMasque = "_";
+    }
+   
 }
 
 
